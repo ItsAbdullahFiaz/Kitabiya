388b5264547f020dca4e114aad3350df2467a8a8
irfanfaizi <irfan_faizi@hashstacktech.com>
2024-11-18 22:21:24 -0800

    image romeve sheet module completed.

---
+++ src/views/screens/add/components/RemoveSheet.tsx
@@ -0,0 +1,61 @@
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import React, {useCallback, useContext, useMemo, useRef} from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import {AppDataContext} from '../../../../context';
+import {useResponsiveDimensions} from '../../../../hooks';
+import {FONT_SIZE, TEXT_STYLE} from '../../../../enums';
+
+export const RemoveSheet = ({
+  handleRemoveModal,
+  removeImage,
+  index
+}: any) => {
+    console.log("INDEX_ON_REMOVE_SHEET===>",index);
+  const {appTheme} = useContext(AppDataContext);
+  const {hp, wp} = useResponsiveDimensions();
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const styles = useMemo(() => {
+    return StyleSheet.create({
+      sheetBtnText: {
+        ...TEXT_STYLE.medium,
+        fontSize: hp(FONT_SIZE.h3),
+        color: appTheme.primaryTextColor,
+      },
+    });
+  }, [appTheme]);
+  return (
+    <GestureHandlerRootView
+      style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.5)'}}>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={['25%']}
+        enablePanDownToClose
+        onClose={() => {
+          console.log('BottomSheet closed');
+          handleRemoveModal(false);
+        }}
+        onChange={handleSheetChanges}
+        backgroundStyle={{backgroundColor: '#fff'}}>
+        <BottomSheetView
+          style={{
+            flex: 1,
+            justifyContent: 'space-around',
+            alignItems: 'flex-start',
+            padding: 20,
+          }}>
+          <TouchableOpacity onPress={()=>removeImage(index)}>
+            <Text style={styles.sheetBtnText}>Remove image</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=>handleRemoveModal(false)}>
+            <Text style={styles.sheetBtnText}>Cancel</Text>
+          </TouchableOpacity>
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
+  );
+};
