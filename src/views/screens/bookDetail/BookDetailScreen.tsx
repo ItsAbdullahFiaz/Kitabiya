82f40eacac06854a33332c3d2bd4440587a7e9b8
irfanfaizi <irfan_faizi@hashstacktech.com>
2024-12-11 20:55:12 +0500

    The ad details screen is updatedd with images slider and better UI.

--- src/views/screens/bookDetail/BookDetailScreen.tsx
+++ src/views/screens/bookDetail/BookDetailScreen.tsx
@@ -1,10 +1,12 @@
 import {
   Image,
   Modal,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
+  useWindowDimensions,
 } from 'react-native';
 import React, {
   useCallback,
@@ -13,7 +15,13 @@ import React, {
   useMemo,
   useState,
 } from 'react';
-import {Header, MainButton, MainContainer} from '../../../components';
+import {
+  AnyIcon,
+  Header,
+  IconType,
+  MainButton,
+  MainContainer,
+} from '../../../components';
 import {FONT_SIZE, OTHER_COLORS, SCREENS, TEXT_STYLE} from '../../../enums';
 import {useResponsiveDimensions} from '../../../hooks';
 import {AppDataContext} from '../../../context';
@@ -22,9 +30,12 @@ import auth from '@react-native-firebase/auth';
 import {apiService} from '../../../services/api';
 import {ReportSheet} from '../add/components';
 import firestore from '@react-native-firebase/firestore';
+import SwiperFlatList from 'react-native-swiper-flatlist';
 
 export const BookDetailScreen = ({route}: any) => {
+  const screenWidth = useWindowDimensions().width;
   const navigation = useNavigation<any>();
+  const [activeIndex, setActiveIndex] = useState(0);
   const {appTheme, appLang} = useContext(AppDataContext);
   const [isExpanded, setIsExpanded] = useState(false);
   const [item, setItem] = useState({});
@@ -33,7 +44,7 @@ export const BookDetailScreen = ({route}: any) => {
   const toggleText = () => setIsExpanded(!isExpanded);
   const {hp, wp} = useResponsiveDimensions();
   const data = route?.params?.product;
-  // console.log('PARAM_DATA_BOOK===>', data);
+  console.log('PARAM_DATA_BOOK===>', data);
   const shouldShowReadMore = data.description.length > 100;
 
   const increseProductView = async () => {
@@ -86,6 +97,9 @@ export const BookDetailScreen = ({route}: any) => {
 
   const styles = useMemo(() => {
     return StyleSheet.create({
+      sliderContainer: {
+        marginTop: hp(20),
+      },
       starsContainer: {
         flexDirection: 'row',
         marginVertical: hp(5),
@@ -152,11 +166,12 @@ export const BookDetailScreen = ({route}: any) => {
         textTransform: 'capitalize',
       },
       detailsContainer: {
-        marginTop: hp(30),
+        marginTop: hp(10),
+        // paddingBottom: hp(100),
       },
       detailHeading: {
         ...TEXT_STYLE.bold,
-        fontSize: hp(FONT_SIZE.h3),
+        fontSize: hp(30),
         color: appTheme.primaryTextColor,
         textTransform: 'capitalize',
       },
@@ -183,56 +198,197 @@ export const BookDetailScreen = ({route}: any) => {
         justifyContent: 'center',
         textAlign: 'center',
       },
+      slideContainer: {
+        height: hp(300),
+        width: screenWidth,
+      },
+      image: {
+        width: '100%',
+        height: '100%',
+      },
+      paginationContainer: {
+        flexDirection: 'row',
+        marginTop: 10,
+        alignSelf: 'center',
+        position: 'absolute',
+        bottom: 0,
+      },
+      activeDot: {
+        backgroundColor: 'white',
+      },
+      inactiveDot: {
+        backgroundColor: 'gray',
+      },
+      paginationStyleItem: {
+        width: hp(12),
+        height: hp(12),
+        borderRadius: 6,
+        marginHorizontal: hp(5),
+      },
+      locationOuterContainer: {
+        marginTop: hp(10),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      },
+      locationInnerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+      },
+      border: {
+        marginVertical: hp(20),
+        width: screenWidth,
+        borderBottomWidth: 0.2,
+        borderBottomColor: appTheme.tertiaryTextColor,
+      },
+      detailKey: {
+        width: '50%',
+        ...TEXT_STYLE.regular,
+        fontSize: hp(FONT_SIZE.h2),
+        color: appTheme.tertiaryTextColor,
+      },
+      detailProp: {
+        width: '50%',
+        ...TEXT_STYLE.medium,
+        fontSize: hp(FONT_SIZE.h2),
+        color: appTheme.primaryTextColor,
+        textTransform: 'capitalize',
+      },
     });
   }, [hp, wp]);
 
   return (
-    <MainContainer>
-      <Header title={appLang.bookdetail} />
-      <View style={styles.introContainer}>
-        <View style={styles.imgContainer}>
-          <Image
-            style={{width: '100%', height: '100%'}}
-            source={{uri: data.images[0]}}
+    <MainContainer fullWidth={true}>
+      <View style={{paddingHorizontal: 16}}>
+        <Header title={appLang.bookdetail} />
+      </View>
+      <View style={styles.sliderContainer}>
+        <SwiperFlatList
+          data={data?.images}
+          renderItem={({item}) => (
+            <View style={styles.slideContainer}>
+              <Image
+                style={styles.image}
+                resizeMode={'cover'}
+                source={{uri: item}}
+              />
+            </View>
+          )}
+          showPagination
+          paginationStyle={styles.paginationContainer}
+          paginationStyleItem={styles.paginationStyleItem}
+          paginationStyleItemInactive={styles.inactiveDot}
+          paginationStyleItemActive={styles.activeDot}
+          onChangeIndex={({index}) => setActiveIndex(index)}
+        />
+      </View>
+      <View style={{flex: 1, padding: 16}}>
+        <ScrollView>
+          <View style={[styles.detailsContainer, {paddingBottom: hp(100)}]}>
+            <Text style={styles.detailHeading}>{`Rs ${data?.price}`}</Text>
+            <Text style={[styles.detailHeading, {fontSize: hp(FONT_SIZE.h1)}]}>
+              {data?.title}
+            </Text>
+            <View style={styles.locationOuterContainer}>
+              <View style={styles.locationInnerContainer}>
+                <AnyIcon
+                  type={IconType.EvilIcons}
+                  name="location"
+                  color={appTheme.primaryTextColor}
+                  size={hp(FONT_SIZE.h1)}
+                />
+                <Text>{data?.locationAddress}</Text>
+              </View>
+              <Text>{`11/12/2024`}</Text>
+            </View>
+            <View style={styles.border} />
+            <View style={styles.textContainer}>
+              <Text
+                style={[styles.detailHeading, {fontSize: hp(FONT_SIZE.h1)}]}>
+                {appLang.details}
+              </Text>
+              <View style={styles.detailsContainer}>
+                <View
+                  style={{
+                    width: '100%',
+                    backgroundColor: 'rgba(0,0,0,0.1)',
+                    height: hp(40),
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    paddingLeft: hp(10),
+                  }}>
+                  <Text style={styles.detailKey}>Type</Text>
+                  <Text style={styles.detailProp}>{data?.type}</Text>
+                </View>
+                <View
+                  style={{
+                    width: '100%',
+                    height: hp(40),
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    paddingLeft: hp(10),
+                  }}>
+                  <Text style={styles.detailKey}>Language</Text>
+                  <Text style={styles.detailProp}>{data?.language}</Text>
+                </View>
+                <View
+                  style={{
+                    width: '100%',
+                    backgroundColor: 'rgba(0,0,0,0.1)',
+                    height: hp(40),
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    paddingLeft: hp(10),
+                  }}>
+                  <Text style={styles.detailKey}>Condition</Text>
+                  <Text style={styles.detailProp}>{data?.condition}</Text>
+                </View>
+                <View
+                  style={{
+                    width: '100%',
+                    height: hp(40),
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    paddingLeft: hp(10),
+                  }}>
+                  <Text style={styles.detailKey}>Is Deliverable</Text>
+                  <Text style={styles.detailProp}>No</Text>
+                </View>
+              </View>
+              <View style={styles.border} />
+              <Text
+                style={[styles.detailHeading, {fontSize: hp(FONT_SIZE.h1)}]}>
+                description
+              </Text>
+              <Text style={styles.text}>
+                {data.description}
+                {!isExpanded && shouldShowReadMore && (
+                  <Text onPress={toggleText} style={styles.readMore}>
+                    {appLang.Readmore}
+                  </Text>
+                )}
+                {isExpanded && shouldShowReadMore && (
+                  <Text onPress={toggleText} style={styles.readMore}>
+                    {appLang.Showless}
+                  </Text>
+                )}
+              </Text>
+            </View>
+            <View style={styles.border} />
+            <TouchableOpacity onPress={() => setIsModalOpen(true)}>
+              <Text style={styles.textcentre}>Add Report</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+        <View style={styles.chatBtnContainer}>
+          <MainButton
+            onPress={() =>
+              navigation.navigate(SCREENS.CHAT as never, {data: item, emailId})
+            }
+            buttonText={appLang.chatnow}
           />
         </View>
-        <View style={styles.nameContainer}>
-          <Text style={styles.name}>{data.title}</Text>
-          {data.price > 0 ? (
-            <Text style={styles.price}>{`$${data.price}`}</Text>
-          ) : (
-            <Text style={styles.free}>{appLang.free}</Text>
-          )}
-        </View>
-      </View>
-      <View style={styles.detailsContainer}>
-        <Text style={styles.detailHeading}>{appLang.details}</Text>
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>
-            {data.description}
-            {!isExpanded && shouldShowReadMore && (
-              <Text onPress={toggleText} style={styles.readMore}>
-                {appLang.Readmore}
-              </Text>
-            )}
-            {isExpanded && shouldShowReadMore && (
-              <Text onPress={toggleText} style={styles.readMore}>
-                {appLang.Showless}
-              </Text>
-            )}
-          </Text>
-        </View>
-        <TouchableOpacity onPress={() => setIsModalOpen(true)}>
-          <Text style={styles.textcentre}>Add Report</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.chatBtnContainer}>
-        <MainButton
-          onPress={() =>
-            navigation.navigate(SCREENS.CHAT as never, {data: item, emailId})
-          }
-          buttonText={appLang.chatnow}
-        />
       </View>
       <Modal visible={isModalOpen}>
         <ReportSheet handleModal={handleModal} product={data} />
