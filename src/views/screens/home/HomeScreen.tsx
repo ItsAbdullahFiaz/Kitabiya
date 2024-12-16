2ca58392ab0daee99878ef16512c1ea4a7a70303
irfanfaizi <irfan_faizi@hashstacktech.com>
2024-12-16 15:22:34 +0500

    skeleton loader on home screen is partially adjusted.

--- src/views/screens/home/HomeScreen.tsx
+++ src/views/screens/home/HomeScreen.tsx
@@ -1,33 +1,29 @@
-import React, { useContext, useEffect, useMemo, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { MainContainer } from '../../../components';
-import { AppDataContext } from '../../../context';
-import { useResponsiveDimensions } from '../../../hooks';
-import { useNavigation } from '@react-navigation/native';
-import { notificationService } from '../../../services/NotificationService';
+import React, {useContext, useEffect, useMemo, useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {MainContainer} from '../../../components';
+import {AppDataContext} from '../../../context';
+import {useResponsiveDimensions} from '../../../hooks';
+import {useNavigation} from '@react-navigation/native';
+import {notificationService} from '../../../services/NotificationService';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { apiService } from '../../../services/api';
-import { Header, LoadingSkeleton, ProductList } from './components';
-import { useProducts } from '../../../hooks/useProducts';
+import {apiService} from '../../../services/api';
+import {Header, LoadingSkeleton, ProductList} from './components';
+import {useProducts} from '../../../hooks/useProducts';
+import {Text} from 'react-native';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<any>();
-  const { appTheme } = useContext(AppDataContext);
-  const { hp, wp } = useResponsiveDimensions();
+  const {appTheme} = useContext(AppDataContext);
+  const {hp, wp} = useResponsiveDimensions();
   const [userName, setUserName] = useState('');
 
-  const {
-    data: products,
-    isLoading,
-    isError,
-    error,
-    refetch
-  } = useProducts();
+  const {data: products, isLoading, isError, error, refetch} = useProducts();
 
   useEffect(() => {
     const setupNotifications = async () => {
       try {
-        const permissionGranted = await notificationService.requestUserPermission();
+        const permissionGranted =
+          await notificationService.requestUserPermission();
         if (permissionGranted) {
           const emailId = await AsyncStorage.getItem('EMAIL');
           if (emailId) {
@@ -62,7 +58,7 @@ export const HomeScreen = () => {
       newlyAddedContainer: {
         paddingHorizontal: hp(16),
         flex: 1,
-      }
+      },
     });
   }, [hp, wp]);
 
--- src/views/screens/home/components/LoadingSkeleton.tsx
+++ src/views/screens/home/components/LoadingSkeleton.tsx
@@ -1,68 +1,94 @@
-import React, { useContext } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { SkeletonLoader } from '../../../../components';
-import { useResponsiveDimensions } from '../../../../hooks';
-import { SIZES } from '../../../../enums';
-import { AppDataContext } from '../../../../context';
+import React, {useContext, useMemo} from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
+import {MainContainer, SkeletonLoader} from '../../../../components';
+import {useResponsiveDimensions} from '../../../../hooks';
+import {AppDataContext} from '../../../../context';
 
 export const LoadingSkeleton = () => {
-    const { hp } = useResponsiveDimensions();
-    const { appTheme } = useContext(AppDataContext);
+  const {hp, wp} = useResponsiveDimensions();
+  const {appTheme} = useContext(AppDataContext);
 
-    return (
-        <View style={styles(hp).container}>
-            {Array.from({ length: 5 }).map((_, index) => (
-                <View key={index} style={styles(hp).card}>
-                    <SkeletonLoader
-                        width={hp(70)}
-                        height={hp(90)}
-                        borderRadius={hp(8)}
-                    />
-                    <View style={styles(hp).textContainer}>
-                        <SkeletonLoader width="60%" height={hp(14)} borderRadius={4} />
-                        <SkeletonLoader
-                            width="40%"
-                            height={hp(12)}
-                            borderRadius={4}
-                            style={{ marginTop: hp(4) }}
-                        />
-                        <SkeletonLoader
-                            width="30%"
-                            height={hp(10)}
-                            borderRadius={4}
-                            style={{ marginTop: hp(6) }}
-                        />
-                    </View>
-                </View>
-            ))}
-        </View>
-    );
-};
-
-const styles = (hp: any) => StyleSheet.create({
-    container: {
-        paddingBottom: hp(50),
-    },
-    card: {
-        marginBottom: hp(24),
+  const styles = useMemo(() => {
+    return StyleSheet.create({
+      container: {
         flex: 1,
+      },
+      card: {
+        height: hp(280),
+        width: wp(170),
         alignItems: 'center',
-        marginHorizontal: hp(17),
-        backgroundColor: 'red',
-        borderRadius: hp(8),
+        backgroundColor: '#A3AAB1', // Adjust color based on your theme
+        borderRadius: 10,
         shadowColor: '#000',
         shadowOffset: {
-            width: 0,
-            height: 2,
+          width: 0,
+          height: 4,
         },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    textContainer: {
-        // flex: 1,
-        width: (SIZES.width / 2) - hp(50),
-        paddingHorizontal: hp(10),
-        paddingVertical: hp(8),
-    },
-});
+        shadowOpacity: 0.3,
+        shadowRadius: 4.65,
+        elevation: 8,
+        marginBottom: hp(5),
+        borderWidth: 0.5,
+        borderColor: appTheme.disabled,
+      },
+      textContainer: {
+        width: '100%',
+        paddingHorizontal: wp(2),
+        paddingVertical: hp(1),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      },
+      userName: {
+        height: hp(30),
+        width: hp(150),
+        marginBottom: hp(2),
+      },
+      title: {
+        height: hp(45),
+        width: hp(200),
+      },
+    });
+  }, [hp, wp, appTheme]);
+
+  return (
+    <MainContainer>
+      <View style={styles.container}>
+        <View style={styles.userName}>
+          <SkeletonLoader
+            width={'100%'}
+            height={'100%'}
+            borderRadius={10}
+            style={{marginBottom: hp(2)}}
+          />
+        </View>
+        <View style={styles.title}>
+          <SkeletonLoader
+            width={'100%'}
+            height={'100%'}
+            borderRadius={10}
+            style={{marginBottom: hp(2)}}
+          />
+        </View>
+        <View style={{marginTop: hp(10)}}>
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6]}
+            renderItem={({item}) => {
+              return (
+                <View style={styles.card}>
+                  <SkeletonLoader
+                    width={'100%'}
+                    height={hp(200)}
+                    borderRadius={10}
+                    style={{marginBottom: hp(2)}}
+                  />
+                </View>
+              );
+            }}
+            numColumns={2}
+            columnWrapperStyle={{justifyContent: 'space-between'}}
+          />
+        </View>
+      </View>
+    </MainContainer>
+  );
+};
